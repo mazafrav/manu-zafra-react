d0dba192dd58040085158c4c651f95fca510b2c8
mazafrav <12997270+mazafrav@users.noreply.github.com>
2026-09-25 13:33:12 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Manu Zafra</h1>
+      <p>Game Designer · Software Engineer</p>
+      <p>New portfolio coming soon.</p>
+    </main>
+  )
+}
+
+export default App
